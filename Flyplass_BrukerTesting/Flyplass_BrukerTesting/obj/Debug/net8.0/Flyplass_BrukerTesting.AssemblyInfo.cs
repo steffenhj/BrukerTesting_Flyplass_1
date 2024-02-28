80cbfe4cb4a255cb4a87906c8a9ae1be2bc35829
notveryhelpfuldude <caracass@hotmail.no>
2024-02-28 14:21:41 +0100

    efa
    efwe

--- Flyplass_BrukerTesting/Flyplass_BrukerTesting/obj/Debug/net8.0/Flyplass_BrukerTesting.AssemblyInfo.cs
+++ Flyplass_BrukerTesting/Flyplass_BrukerTesting/obj/Debug/net8.0/Flyplass_BrukerTesting.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Flyplass_BrukerTesting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7f7bd501d34bfc0e14de90c57f9e8f5b98fc6cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Flyplass_BrukerTesting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Flyplass_BrukerTesting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
